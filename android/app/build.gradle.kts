4f17caf393e588274848811d26085c3908f130b3
CKYA <cyrillthegreat0@gmail.com>
2025-09-30 23:22:00 +0530

    Your changes

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,15 +7,9 @@ plugins {
 
 android {
     namespace = "moe.ckya.cutetask"
-    ndkVersion = "27.0.12077973"
-    compileSdk = 33
-    defaultConfig {
-        applicationId = "moe.ckya.cutetask"
-        minSdk = 23
-        targetSdk = 33
-        versionCode = 1
-        versionName = "1.0"
-    }
+    compileSdk = flutter.compileSdkVersion
+    ndkVersion = flutter.ndkVersion
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
@@ -27,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.cute_task"
+        applicationId = "com.example.blackbase"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
